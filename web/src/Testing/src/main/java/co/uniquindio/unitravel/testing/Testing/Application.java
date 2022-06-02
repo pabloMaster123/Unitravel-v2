1d384e3e4e50804ddc7759d61fcefa5598a726bb
i7Savage <santiago.povedag@uqvirtual.edu.co>
2022-06-02 15:01:12 -0500

    Testing Automatizacion Selenium WebDriver+Junit5

---
+++ web/src/Testing/src/main/java/co/uniquindio/unitravel/testing/Testing/Application.java
@@ -0,0 +1,13 @@
+package co.uniquindio.unitravel.testing.Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
